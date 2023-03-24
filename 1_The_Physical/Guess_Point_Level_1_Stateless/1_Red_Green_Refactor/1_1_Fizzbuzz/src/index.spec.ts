8a850e3670e1c7aa3cb22d692363ffce4993f950
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-24 12:31:42 +0100

    [new] fizzbuzz - takes numbers from 1 to 100

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,13 @@
+import { fizzBuzz } from './fizzbuzz'
 
-describe("fizzbuzz", () => {
+describe('fizzbuzz', () => {
 
+    it('takes numbers from 1 to 100', () => {
+        expect(() => fizzBuzz(0)).toThrow('The number must be between 1 and 100.');
+        expect(() => fizzBuzz(1)).not.toThrow('The number must be between 1 and 100.');
+        expect(() => fizzBuzz(50)).not.toThrow('The number must be between 1 and 100.');
+        expect(() => fizzBuzz(100)).not.toThrow('The number must be between 1 and 100.');
+        expect(() => fizzBuzz(101)).toThrow('The number must be between 1 and 100.');
+    })
 
 });
