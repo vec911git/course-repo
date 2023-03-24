6c0f9791a8d87a47f2640276ebb6e8b75a898e8b
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-24 20:40:16 +0100

    [new] palindrome - Create a palindrome checker

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,7 @@
+import { isPalindrome } from './index'
 
 describe('palindrome checker', () => {
-
+    it('Create a palindrome checker', () => {
+        expect(typeof(isPalindrome)).toBe("function");
+    })
 })
