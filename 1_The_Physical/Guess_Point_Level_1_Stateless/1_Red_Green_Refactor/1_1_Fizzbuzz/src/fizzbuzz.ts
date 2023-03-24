40af6eb1354002fc9fc0e40fc9f69e8361a06b5f
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-24 13:32:12 +0100

    [refact] fizzbuzz - Improved readalbility

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,14 +2,10 @@ export function fizzBuzz(number: number) {
     if (number < 1 || number > 100)
         throw new Error('The number must be between 1 and 100.');
     
-    if (number%3 == 0 && number%5 == 0)
-        return 'FizzBuzz.';
-
-    if (number%3 == 0)
-        return 'Fizz';
-
-    if (number%5 == 0)
-        return 'Buzz.';
-
-    return number.toString();
+    let word = '';
+    if (number%3 == 0) word = word.concat('Fizz');
+    if (number%5 == 0) word = word.concat('Buzz.');
+    if (word.length === 0) word = number.toString();
+        
+    return word;
 }
